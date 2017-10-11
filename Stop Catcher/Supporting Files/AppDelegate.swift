4f3a647f66050d6ece4e6e08b5b1f73ba360c1d5
Brian <brianmartinmurphy@gmail.com>
2017-10-10 22:13:54 -0400

    - Removing Fabric + Crashlytics as it is old and outdated

--- Stop Catcher/Supporting Files/AppDelegate.swift
+++ Stop Catcher/Supporting Files/AppDelegate.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
-        Fabric.with([Crashlytics.self])
+//        Fabric.with([Crashlytics.self])
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
