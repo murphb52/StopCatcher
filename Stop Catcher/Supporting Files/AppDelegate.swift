0d2f7e767e6179dae3098815d6c960117c872e99
Brian <brianmartinmurphy@gmail.com>
2017-10-15 13:40:11 -0400

    - Removing comments around fabric

--- Stop Catcher/Supporting Files/AppDelegate.swift
+++ Stop Catcher/Supporting Files/AppDelegate.swift
@@ -17,8 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
-//        Fabric.with([Crashlytics.self])
-
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
         rootViewController = SCMainViewController(nibName: "SCMainViewController", bundle: nil)
